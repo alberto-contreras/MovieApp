4f205141c0bf5ef92d9afe063863c8d2513d919d
alberto-contreras <contreras.m.alberto@gmail.com>
2020-01-30 17:07:07 +0100

    movie-detail page added

--- src/app/pages/movie-details/movie-details.page.ts
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ActivatedRoute} from '@angular/router'
+import { SearchType } from "src/app/services/movie.service";
+import { MovieService } from "./../../services/movie.service"
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.page.html',
   styleUrls: ['./movie-details.page.scss'],
 })
 export class MovieDetailsPage implements OnInit {
+  information = null;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private movieService: MovieService) {}
 
   ngOnInit() {
+    //We get the paremeter that movie page pass an call movieService to search the details
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+
+    this.movieService.getDetails(id).subscribe(result =>{
+      console.log('details: ', result);
+      this.information = result;
+    });
   }
+  //openWebsite it's to open when everything it's loaded
+
 
 }
